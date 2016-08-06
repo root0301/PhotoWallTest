a245ff951836eb3a623b7e0a52746f35cc17d99b
root0301 <2648398526@qq.com>
2016-08-06 21:45:48 +0800

    RecyclerView结合Glide

--- app/src/main/java/photo/example/com/photowalltest/MainActivity.java
+++ app/src/main/java/photo/example/com/photowalltest/MainActivity.java
@@ -2,6 +2,8 @@ package photo.example.com.photowalltest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.widget.GridView;
 
@@ -12,21 +14,33 @@ public class MainActivity extends Activity {
 
     private GridView photoWall;
 
-    private PhotoWallAdapter adapter;
+/*    private PhotoWallAdapter adapter;*/
+
+    private RecyclerView recyclerView;
+
+    private PhotoRecyclerAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        photoWall = (GridView) findViewById(R.id.photo_wall);
+        initRecyclerView();
+/*        photoWall = (GridView) findViewById(R.id.photo_wall);
         adapter = new PhotoWallAdapter(this,0, Images.allImageUrls, photoWall);
-        photoWall.setAdapter(adapter);
+        photoWall.setAdapter(adapter);*/
+    }
+
+    private void initRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler);
+        adapter = new PhotoRecyclerAdapter(this);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        adapter.cancelAllTask();
+/*        adapter.cancelAllTask();*/
     }
 }
--- app/src/main/java/photo/example/com/photowalltest/PhotoRecyclerAdapter.java
+++ app/src/main/java/photo/example/com/photowalltest/PhotoRecyclerAdapter.java
@@ -1,12 +1,19 @@
 package photo.example.com.photowalltest;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 /**
  * Created by slience on 2016/8/5.
@@ -17,9 +24,27 @@ public class PhotoRecyclerAdapter extends RecyclerView.Adapter<PhotoRecyclerAdap
 
     private LayoutInflater mLayoutInflater;
 
+    private LruCache<String, Bitmap> mLruCache;
+
+    private String mCu=null;
+
+    private String[] mUrl;
+    private String TAG = "DEBUG";
+
     public PhotoRecyclerAdapter(Context context) {
         mContext = context;
         mLayoutInflater = LayoutInflater.from(context);
+        mUrl = Images.allImageUrls;
+        int maxMemory = (int) Runtime.getRuntime().maxMemory();
+        int cacheSize = maxMemory/8;
+        mLruCache = new LruCache<String,Bitmap>(cacheSize){
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getByteCount();
+            }
+        };
+        Log.d(TAG,"构造方法");
+        Log.d(TAG, String.valueOf(mUrl.length));
     }
 
     @Override
@@ -29,21 +54,57 @@ public class PhotoRecyclerAdapter extends RecyclerView.Adapter<PhotoRecyclerAdap
 
     @Override
     public void onBindViewHolder(PhotoRecyclerHolder holder, int position) {
+        mCu = mUrl[position];
+        Bitmap bitmap = getBitmapFromCache(mUrl[position]);
+        if (bitmap != null) {
+            holder.imge.setImageBitmap(bitmap);
+        } else {
+            Glide.with(mContext)
+                    .load(mUrl[position])
+                    .fitCenter()
+                    .into(holder.imge);
+            Glide.with(mContext).load(mUrl[position]).asBitmap().into(target);
+        }
 
     }
 
+
+    public SimpleTarget target = new SimpleTarget<Bitmap>() {
+        @Override
+        public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+            // do something with the bitmap
+            // for demonstration purposes, let's just set it to an ImageView
+            addBitmapToCache(mCu,bitmap);
+        }
+    };
+
+
+    //添加图片到LruCache
+    public void addBitmapToCache(String key, Bitmap bitmap) {
+        if(getBitmapFromCache(key) == null) {
+            mLruCache.put(key, bitmap);
+        }
+    }
+
+
+    //从LruCache中取图片
+    public Bitmap getBitmapFromCache(String url) {
+        return mLruCache.get(url);
+    }
+
+
     @Override
     public int getItemCount() {
-        return 0;
+        return mUrl.length;
     }
 
     public class PhotoRecyclerHolder extends RecyclerView.ViewHolder{
 
-        CardView cardView;
+        ImageView imge;
 
         public PhotoRecyclerHolder(View itemView) {
             super(itemView);
-            cardView = (CardView) itemView.findViewById(R.id.item);
+            imge= (ImageView) itemView.findViewById(R.id.item_photo);
         }
     }
 }
